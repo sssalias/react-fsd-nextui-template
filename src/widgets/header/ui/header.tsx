f7cea66701e78394b1754eed744720a06437358c
Alex Efremenkov <top.chel.05@mail.ru>
2025-02-12 11:13:52 +0300

    created: header

---
+++ src/widgets/header/ui/header.tsx
@@ -0,0 +1,18 @@
+import { Container } from 'src/shared/ui'
+
+const Header: React.FC = () => {
+    return (
+        // There is header
+        <header>
+            {/* There is container */}
+            <Container>
+                {/* There is wrapper */}
+                <div>
+                    
+                </div>
+            </Container>
+        </header>
+    )
+}
+
+export default Header
